7aa0ec271c29be5d249a59d1f6c7d8bbe5635cb6
Tyler Viducic <tyler.viducic@gmail.com>
2020-07-24 09:11:17 -0400

    cleaned up dataManager

--- aidapt_fiducial/dataManager.py
+++ aidapt_fiducial/dataManager.py
@@ -3,67 +3,52 @@
 import numpy as np
 import time
 
-# with open('/media/tylerviducic/Elements/aidapt/data/recon/twopi_ppip.10.zzz') as f:
-#     all_lines = f.readlines()
-
-# for i in range(10):
-#     print(all_lines[i])
 
 class DataManager:
 
     def __init__(self, file):
+        
         self.file = file
     
     def get_numpy_array(self):
         if self.file.endswith('.npy'):
+        
             print('Data loaded')
             print('Applying fiducial cuts')
+        
             return np.load(self.file)
+        
         elif self.file.endswith('.zzz') or self.file.endswith('.txt'):
+        
             print('Getting numpy array from .txt file data')
             array = self._get_numpy_array_from_zzz() 
             print('Applying fiducial cuts')
+        
             return array
 
 
     # Private Methods
 
     def _get_numpy_array_from_zzz(self):
+        
         output_list = []
         
         with open(self.file) as f:
             all_lines = f.readlines()
         
         new_row = []
-        start = time.time()
-        for i in range(len(all_lines)):
         
-            for entry in all_lines[i].split(" "):
+        for line in all_lines:
+            for entry in line.split(" "):
                 try: 
                     new_row.append(float(entry))
                 except ValueError:
                     pass
-            if (i + 1) % 5 == 0:
+            
+            if len(new_row) == 14:
+            
                 output_list.append(new_row)
                 new_row = []
-                end = time.time()
-                row_time = end - start
-                start = time.time()
-
-                if((i + 1) % 1000000 == 0):
-                    print('Done reading {} % of file - time/row = {}'.format(i/len(all_lines) * 100, row_time))
 
         return np.array(output_list)
 
-
-
-
-# test_file = '/media/tylerviducic/Elements/aidapt/data/recon/twopi_ppip.10.zzz'
-# test_manager = DataManager(test_file)
-
-# start = time.time()
-# my_array = test_manager.get_numpy_array()
-# end = time.time()
-
-# print('Array length: {}'.format(len(my_array)))
-# print("time was " + str(end - start))
--- aidapt_fiducial/fiducialCuts.py
+++ aidapt_fiducial/fiducialCuts.py
@@ -5,11 +5,12 @@ import math
 class FiducialCuts:
 
     def __init__(self, event, missing_pi_minus=True):
+        
         self.missing_pi_minus = missing_pi_minus
         self.event = event
         self.phi_cut = 26.0 # in Degrees
-        self.theta_cut = 0.985
-        self.momentum_cut = 0.375 
+        self.costheta_cut = 0.985
+        self.momentum_cut = 0.375 # in GeV
 
     def check_event_pass(self):
         return self._check_if_event_pass_momentum() and self._check_if_event_pass_phi() and self._check_if_event_pass_theta()
@@ -20,20 +21,24 @@ class FiducialCuts:
         return math.degrees(phi) < abs(self.phi_cut)
 
     def _passes_theta(self, theta):
-        return math.cos(theta) < self.theta_cut
+        return math.cos(theta) < self.costheta_cut
 
     def _passes_momentum(self, momentum):
         return momentum > self.momentum_cut
 
     def _check_if_event_pass_phi(self):
+        
         if self.missing_pi_minus:
             return self._passes_phi(self.event.get_proton_phi()) and self._passes_phi(self.event.get_pi_plus_phi())
+        
         else: 
              return self._passes_phi(self.event.get_proton_phi()) and self._passes_phi(self.event.get_pi_plus_phi()) and self._passes_phi(self.event.get_pi_minus_phi())
 
     def _check_if_event_pass_theta(self):
+        
         if self.missing_pi_minus:
             return self._passes_theta(self.event.get_proton_theta()) and self._passes_theta(self.event.get_pi_plus_theta())
+        
         else: 
              return self._passes_theta(self.event.get_proton_theta()) and self._passes_theta(self.event.get_pi_plus_theta()) and self._passes_theta(self.event.get_pi_minus_theta())
 
